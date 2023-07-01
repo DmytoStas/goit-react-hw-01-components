1502235205fe3b877edbf5102c9bc62701ee438f
Dmytro Stas <dima7stas777@gmail.com>
2023-06-30 22:02:56 -0400

    Fix random hex color function

--- src/js/getRandomColor.js
+++ src/js/getRandomColor.js
@@ -1,5 +1,6 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  let n = (Math.random() * 0xfffff * 1000000).toString(16);
+  return '#' + n.slice(0, 6);
 }
 
 export default function getRandomColor() {
